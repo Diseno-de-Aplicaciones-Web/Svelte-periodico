c1bd5ec7a01965d0f8905c6bc47b976995f0120c
Daniel Bañobre Dopico <bytes4life@proton.me>
2023-03-13 12:27:30 +0100

    Provides nav and conditional rendering

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,54 @@
+<nav>
+    <ul>
+        <li>
+            <button on:click={manejadorNavNoticias}>
+                Noticias
+            </button>
+        </li>
+        <li>
+            <button on:click={manejadorNavFormulario}>
+                Formulario
+            </button>
+        </li>
+    </ul>
+</nav>
 
-<h1>Novas</h1>
+{#if (vistaActual === "noticias")}
 
-{#each novas as estaNova}
-    <Noticias
-        titulo={estaNova.titulo}
-        contido={estaNova.contido}
-        fonteImaxe={estaNova.fonteDaImaxe}
-        textoAlternativoImaxe={estaNova.textoAlternativo}
-    />
-{/each}
+    <h1>Novas</h1>
 
-<h1>Formulario</h1>
-<Formulario bind:listaNovas={novas}/>
+    {#each novas as estaNova}
+        <Noticias
+            titulo={estaNova.titulo}
+            contido={estaNova.contido}
+            fonteImaxe={estaNova.fonteDaImaxe}
+            textoAlternativoImaxe={estaNova.textoAlternativo}
+        />
+    {/each}
 
+{/if}
+
+{#if (vistaActual === "formulario")}
+
+    <h1>Formulario</h1>
+    <Formulario bind:listaNovas={novas}/>
+
+{/if}
 
 <script>
 
     import Noticias from "./conponents/Noticias/Noticias.svelte";
     import Formulario from "./conponents/Formulario/Formulario.svelte";
+
     let novas = []
+    let vistaActual = "noticias"
+
+    function manejadorNavNoticias() {
+        vistaActual = "noticias"
+    }
+
+    function manejadorNavFormulario() {
+        vistaActual = "formulario"
+    }
 
 </script>
